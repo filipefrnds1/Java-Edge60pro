1e9ff56d7c067a37de30e0f8ddfb59bba9f3ef05
filipefrnda1 <filipefirmino50@gmail.com>
2026-05-12 23:04:49 -0300

    just commit today

--- JavaAgain.java
+++ JavaAgain.java
@@ -1,5 +1,7 @@
 public class JavaAgain {
     public static void main(String[] args) {
         System.out.println("Hello World JavaAgain Testing");
-    }
+   	System.out.println("Just Commit today");
+
+	 }
 }
